af974660ce9ad3c5e809fdc61316b4fbdb41f406
Yoti <realyoti@gmail.com>
2016-11-04 20:46:38 +0300

    [+] Joyetech (rev5)

--- joyetech/firmscan_vtwomini.bat
+++ joyetech/firmscan_vtwomini.bat
@@ -1,6 +1,8 @@
 @echo off
 title VTwo Mini
 
+echo %date:~6,4%%date:~3,2%%date:~0,2%
+firmscan\wget -q -O "UpdateFirmware_%date:~6,4%%date:~3,2%%date:~0,2%.zip" "http://www.joyetech.com/wp-content/uploads/2016/05/evicvtwomini/win/UpdateFirmware.zip" 2> NUL
 for /l %%i in (0,1,9) do (
 	for /l %%j in (0,1,9) do (
 		for /l %%k in (0,1,9) do (
@@ -14,8 +16,6 @@ for /l %%i in (0,1,9) do (
 	)
 
 )
-echo %date:~6,4%%date:~3,2%%date:~0,2%
-firmscan\wget -q -O "UpdateFirmware_%date:~6,4%%date:~3,2%%date:~0,2%.zip" "http://www.joyetech.com/wp-content/uploads/2016/05/evicvtwomini/win/UpdateFirmware.zip" 2> NUL
 for %%F in (UpdateFirmware_*.zip) do if %%~zF equ 0 del "%%F"
 
 pause
