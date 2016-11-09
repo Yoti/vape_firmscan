d6d0e484dee22b04108b0c84ad1656327526a24a
Yoti <realyoti@gmail.com>
2016-11-09 19:48:19 +0300

    [+] Wismec (rev4)

--- wismec/firmscan_rx2-3.bat
+++ wismec/firmscan_rx2-3.bat
@@ -1,6 +1,7 @@
 @echo off
 title RX2-3
 
+if exist %0.log del /q %0.log
 rem http://www.wismec.com/software/wismec-tc-software-for-upgrading-reuleaux-rx23/
 for /l %%i in (0,1,9) do (
 	for /l %%j in (0,1,9) do (
@@ -8,11 +9,17 @@ for /l %%i in (0,1,9) do (
 			echo V%%i.%%j%%k
 			if not exist "Reuleaux_RX23_V%%i.%%j%%k.zip" (
 				firmscan\wget -q "http://www.wismec.com/images/support/download/Reuleaux-RX23/Reuleaux_RX23_V%%i.%%j%%k.zip" 2> NUL
-				if exist "Reuleaux_RX23_V%%i.%%j%%k.zip" echo + Reuleaux_RX23_V%%i.%%j%%k.zip
+				if exist "Reuleaux_RX23_V%%i.%%j%%k.zip" (
+					echo + Reuleaux_RX23_V%%i.%%j%%k.zip
+					echo Reuleaux_RX23_V%%i.%%j%%k.zip >> %0.log
+				)
 			)
 		)
 	)
 
 )
+cls
+if exist %0.log type %0.log
+if exist %0.log del /q %0.log
 
 pause
--- wismec/firmscan_rx200.bat
+++ wismec/firmscan_rx200.bat
@@ -1,6 +1,7 @@
 @echo off
 title RX200
 
+if exist %0.log del /q %0.log
 rem http://www.wismec.com/software/wismec-tc-software-for-upgrading-reuleaux-rx200/
 for /l %%i in (0,1,9) do (
 	for /l %%j in (0,1,9) do (
@@ -8,15 +9,24 @@ for /l %%i in (0,1,9) do (
 			echo V%%i.%%j%%k
 			if not exist "Reuleaux_RX200_V%%i.%%j%%k.zip" (
 				firmscan\wget -q "http://www.wismec.com/download/rx200/V%%i_%%j%%k/Reuleaux_RX200_V%%i.%%j%%k.zip" 2> NUL
-				if exist "Reuleaux_RX200_V%%i.%%j%%k.zip" echo + Reuleaux_RX200_V%%i.%%j%%k.zip
+				if exist "Reuleaux_RX200_V%%i.%%j%%k.zip" (
+					echo + Reuleaux_RX200_V%%i.%%j%%k.zip
+					echo Reuleaux_RX200_V%%i.%%j%%k.zip >> %0.log
+				)
 			)
 			if not exist "Reuleaux_RX200_V%%i.%%j%%k.zip" (
 				firmscan\wget -q "http://www.wismec.com/images/support/download/RX200/Reuleaux_RX200_V%%i.%%j%%k.zip" 2> NUL
-				if exist "Reuleaux_RX200_V%%i.%%j%%k.zip" echo + Reuleaux_RX200_V%%i.%%j%%k.zip
+				if exist "Reuleaux_RX200_V%%i.%%j%%k.zip" (
+					echo + Reuleaux_RX200_V%%i.%%j%%k.zip
+					echo Reuleaux_RX200_V%%i.%%j%%k.zip >> %0.log
+				)
 			)
 		)
 	)
 
 )
+cls
+if exist %0.log type %0.log
+if exist %0.log del /q %0.log
 
 pause
